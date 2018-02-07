a52fc4e1c5bfb1aa5aa4380cd00c13059e0ab756
Rhawan Brenner <rb.brenner10@gmail.com>
2018-02-07 14:27:53 -0200

    Deploy da API no heroku

--- algamoney-api/src/test/java/com/algamoneyapi/AlgamoneyApiApplicationTests.java
+++ algamoney-api/src/test/java/com/algamoneyapi/AlgamoneyApiApplicationTests.java
@@ -5,8 +5,10 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.algamoney.api.AlgamoneyApiApplication;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = AlgamoneyApiApplication.class)
 public class AlgamoneyApiApplicationTests {
 
 	@Test
