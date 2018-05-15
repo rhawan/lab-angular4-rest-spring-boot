33e3c3a1a2422df69fe82106c8652d9feb4840ed
Rhawan <rhawan.franco@maximasistemas.com.br>
2018-05-15 13:30:12 -0300

    Implementando projetos de exemplo com Angular

---
+++ exibindo-condicionalmente-com-as-diretivas-ngif-e-hidden/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  nome = 'Rhawan';
+  adicionado = false;
+
+  adicionar() {
+    console.log(`Adicionando ${this.nome}`);
+    this.adicionado = true;
+
+    setTimeout(() => {
+      this.adicionado = false;
+    }, 5000);
+  }
+
+}
