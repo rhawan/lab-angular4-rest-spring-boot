538880683fdb8a34ace30e15c8acfaa78098a1ee
Rhawan <rhawan.franco@maximasistemas.com.br>
2018-05-17 09:18:56 -0300

    Implementando projetos de exemplo com Angular

---
+++ binding-de-eventos-customizados-com-output-e-eventemitter/src/app/funcionario-card/funcionario-card.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-funcionario-card',
+  templateUrl: './funcionario-card.component.html',
+  styleUrls: ['./funcionario-card.component.css']
+})
+export class FuncionarioCardComponent implements OnInit {
+
+  @Input() funcionario: any;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ passando-dados-com-input/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { FuncionarioCardComponent } from './funcionario-card/funcionario-card.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FuncionarioCardComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+	
