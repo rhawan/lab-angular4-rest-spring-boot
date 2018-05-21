6471f9436df8aeb6723c1441415f27434971690c
Rhawan <rhawan.franco@maximasistemas.com.br>
2018-05-21 13:07:53 -0300

    Implementando projetos de exemplo com Angular

---
+++ adicionando-estilos-css-em-componentes/src/app/funcionario-card/funcionario-card.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-funcionario-card',
+  templateUrl: './funcionario-card.component.html',
+  //styleUrls: ['./funcionario-card.component.css']
+  styles: [`
+    .card-block {
+      text-transform: uppercase;
+      color: blue;
+    }
+  `]
+})
+export class FuncionarioCardComponent implements OnInit {
+
+  @Input() funcionario: any;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
