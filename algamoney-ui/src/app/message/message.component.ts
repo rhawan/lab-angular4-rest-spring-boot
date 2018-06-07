e262632fef752304f14cc560132cf4a021b7a73d
Rhawan <rhawan.franco@maximasistemas.com.br>
2018-06-07 14:13:31 -0300

    Implementando projeto algamoney-ui

--- algamoney-ui/src/app/message/message.component.ts
+++ algamoney-ui/src/app/message/message.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FormControl  } from '@angular/forms';
 
 @Component({
   selector: 'app-message',
   template: `
-    <p>
-      message works!
-    </p>
+    <div *ngIf="temErro()"
+      class="ui-message ui-messages-error">
+        {{ text }}
+    </div>
   `,
-  styles: []
+  styles: [`
+    .ui-messages-error {
+      margin: 0;
+      margin-top: 4px;
+    }
+  `]
 })
 export class MessageComponent {
+
+  @Input() error: string;
+  @Input() control: FormControl;
+  @Input() text: string;
+
+  temErro(): boolean {
+    return this.control.hasError(this.error) && this.control.dirty;
+  }
 
 
 }
