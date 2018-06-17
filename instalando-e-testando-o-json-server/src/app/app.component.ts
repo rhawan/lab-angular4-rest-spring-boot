704b8754e69a0c3d681df5ae8dc20cc41db8ea15
Rhawan <rhawan.franco@maximasistemas.com.br>
2018-06-17 18:46:11 -0300

    Utilizando módulo http do Angular

---
+++ instalando-e-testando-o-json-server/src/app/app.component.ts
@@ -0,0 +1,52 @@
+import { CidadeService } from './cidade.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+
+  cidades = [];
+
+  constructor(private cidadeService: CidadeService) { }
+
+  ngOnInit() {
+    this.consultar();
+  }
+
+  consultar() {
+    this.cidadeService.consultar()
+    .then(dados => {
+      this.cidades = dados;
+    });
+  }
+
+  adicionar(nome: string) {
+    this.cidadeService.adicionar({ nome: nome })
+    .then(cidade => {
+      alert(`Cidade ${cidade.nome} adicionada com código ${cidade.id}`);
+      this.consultar();
+    });
+  }
+
+  excluir(id: number) {
+    this.cidadeService.excluir(id)
+    .then(() => {
+      alert('Cidade excluída');
+      this.consultar();
+    });
+  }
+
+  atualizar(cidade: any) {
+    this.cidadeService.atualizar(cidade)
+    .then(() => {
+      alert('Cidade alterada com sucesso!');
+    })
+    .catch(erro => {
+      alert(erro);
+    });
+  }
+
+}
